3bdff9b104c7afa5f0c9b4c368cf834d0c1657eb
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-06 06:20:06 -0500

    Delete guitarra.py

--- guitarra.py
+++
@@ -1,6 +0,0 @@
-from instrumentos import Instrumento
-class Guitarra(Instrumento):
-    def afinar():
-        print("La guitarra esta afinada")
-    def tocar():
-        print("se esta tocando la guitarra")
