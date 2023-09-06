4d26eef278464c8bd229689b54e6ff7ee06a1d7b
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-06 06:17:31 -0500

    Delete maracas.py

--- maracas.py
+++
@@ -1,6 +0,0 @@
-from instrumentos import Instrumento
-class Maracas(Instrumento):
-    def afinar():
-        print("Las maracas no necesitan ser afinadas")
-    def tocar():
-        print("Se estan tocando las maracas")
