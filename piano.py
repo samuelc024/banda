d02ebd67bd9b292fe18fc08a744a063121a3d154
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-06 06:16:29 -0500

    Delete piano.py

--- piano.py
+++
@@ -1,6 +0,0 @@
-from instrumentos import Instrumento
-class Piano(Instrumento):
-    def afinar():
-        print("El piano esta afinado")
-    def tocar():
-        print("Se esta tocando el piano")
