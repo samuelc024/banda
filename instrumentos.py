28245f5de57aafb7f5ff4e93335b314f3025ea8e
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-06 06:19:41 -0500

    Update instrumentos.py

--- instrumentos.py
+++ instrumentos.py
@@ -3,6 +3,17 @@ class Instrumento:
         pass
     def tocar(self):
         pass
+    class Maracas(Instrumento):
+        def afinar(self):
+            print("Las maracas no necesitan ser afinadas y ")
+        def tocar(self):
+            print("se estan tocando las maracas")
+    class Piano(Instrumento):
+        def afinar(self):
+            print("El piano no necesita ser afinado")
+        def tocar(self):
+            print("se estan tocando el piano")
+    
 
 if __name__ =='__main__':
     u = Instrumento()
