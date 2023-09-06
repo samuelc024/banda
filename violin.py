d2757913a164e6850eaaae5342c0bcb2311f6db5
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-06 06:22:09 -0500

    Delete violin.py

--- violin.py
+++
@@ -1,6 +0,0 @@
-from instrumentos import Instrumento
-class Violin(Instrumento):
-    def afinar():
-        print("El violin esta afinado")
-    def tocar():
-        print("Se esta tocando el violin")
