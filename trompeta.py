7f3f7948147d4a984bba930f3f625d7cd2b312a2
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-06 06:21:17 -0500

    Delete trompeta.py

--- trompeta.py
+++
@@ -1,6 +0,0 @@
-from instrumentos import Instrumento
-class Trompeta(Instrumento):
-    def afinar():
-        print("La tromepta esta afinada")
-    def tocar():
-        print("Se esta tocando la tompeta")
